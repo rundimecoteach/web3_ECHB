ca67706f13ca685071bc8bea9f6ece09afa1334f
Hrafven <hrafven@gmail.com>
2019-11-19 15:17:58 +0000

    Add JT and BP and depedencies

--- src/ex1.py
+++ src/ex1.py
@@ -1,10 +1,14 @@
-import os
+import errno
+import pickle
 import glob
+import os
+import sys
 
 import justext
 import requests
 from boilerpipe.extract import Extractor
 from bs4 import BeautifulSoup
+from numpy import mean, std
 
 
 def get_files(path):
@@ -26,7 +30,15 @@ def get_files(path):
     return texts
 
 
+def getMetrics(text):
+    res = dict()
+    res['lineCount'] = str(text).count('\n')
+    res['charCount'] = len(str(text))
+    return res
+
+
 def justextProcess(data):
+    outData = dict()
     out_path = '../resources/JT/'
 
     if not os.path.exists(os.path.dirname(out_path)):
@@ -46,8 +58,8 @@ def justextProcess(data):
         )
         for paragraph in paragraphs:
             if not paragraph.is_boilerplate:
-                content += str(paragraph.text) if str(paragraph.text).find(
-                    '<p>') else '<p>{}</p>'.format(str(paragraph.text))
+                content += str(paragraph.text) if '<p>' in str(
+                    paragraph.text) else '<p>{}</p>'.format(str(paragraph.text))
         if content != '':
             out_file = open(
                 out_path + os.path.basename(filename),
@@ -56,8 +68,105 @@ def justextProcess(data):
             out_file.write(content)
             out_file.close()
             nWrite += 1
+            outData[os.path.basename(filename)] = getMetrics(content)
 
     print('w#{}/{}'.format(nWrite, nTotal))
+    return outData
+
+
+def boilerpipeProcess(data):
+    outData = dict()
+    out_path = '../resources/BP/'
+
+    if not os.path.exists(os.path.dirname(out_path)):
+        try:
+            os.makedirs(os.path.dirname(out_path))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    nTotal = len(data.keys())
+    nWrite = 0
+
+    content = ''
+    filenames = list(data.keys())
+    for filename in filenames:
+        abs_path = os.path.abspath(filename)
+        os.rename(abs_path, abs_path + '.html')
+        extractor = Extractor(
+            extractor='ArticleExtractor',
+            url='file://{}.html'.format(abs_path)
+        )
+        extracted_text = extractor.getText()
+        for paragraph in extracted_text.split('\n'):
+            content += str(paragraph) if '<p>' in str(
+                paragraph) else '<p>{}</p>\n'.format(str(paragraph))
+        if content != '':
+            out_file = open(
+                out_path + os.path.basename(filename),
+                mode='w+', encoding='utf-8'
+            )
+            out_file.write(content)
+            out_file.close()
+            nWrite += 1
+            outData[os.path.basename(filename)] = getMetrics(content)
+        os.rename(abs_path + '.html', abs_path)
+    print('w#{}/{}'.format(nWrite, nTotal))
+    return outData
+
+
+def computeStastiscalMetrics(metricsRef, metrics):
+    res = dict()
+
+    def computeGlobal(metricDict):
+        res = dict()
+        totalLine = 0
+        totalChar = 0
+        for filename, metric in metricDict.items():
+            totalLine += metric['lineCount']
+            totalChar += metric['charCount']
+        lineMean = totalLine / len(metricDict.keys())
+        charMean = totalChar / len(metricDict.keys())
+        lineStd = std(
+            list(map(lambda x: x['lineCount'], metricDict.values())))
+        charStd = std(
+            list(map(lambda x: x['charCount'], metricDict.values())))
+
+        res['totalLine'] = totalLine
+        res['totalChar'] = totalChar
+        res['lineMean'] = lineMean
+        res['charMean'] = charMean
+        res['lineStd'] = lineStd
+        res['charStd'] = charStd
+        return res
+
+    def compare(metricsRef, metric, filename):
+        res = dict()
+        refMetric = metricsRef[filename.replace('.html', '')]
+        res['diffLine'] = abs(refMetric['lineCount'] - metric['lineCount'])
+        res['diffChar'] = abs(refMetric['charCount'] - metric['charCount'])
+        return res
+
+    ref = computeGlobal(metricsRef)
+    if metrics is not None:
+        metricsRes = computeGlobal(metrics)
+        list_diff = list()
+        for filename, metric in metrics.items():
+            list_diff.append(compare(metricsRef, metric, filename))
+        diffLineMean = mean(list(map(lambda x: x['diffLine'], list_diff)))
+        diffCharMean = mean(list(map(lambda x: x['diffChar'], list_diff)))
+
+        diffLineStd = std(list(map(lambda x: x['diffLine'], list_diff)))
+        diffCharStd = std(list(map(lambda x: x['diffChar'], list_diff)))
+
+        res['metrics'] = metricsRes
+        res['diffLineMean'] = diffLineMean
+        res['diffCharMean'] = diffCharMean
+        res['diffLineStd'] = diffLineStd
+        res['diffCharStd'] = diffCharStd
+
+    res['ref'] = ref
+    return res
 
 
 if __name__ == "__main__":
@@ -67,4 +176,36 @@ if __name__ == "__main__":
     htmls = get_files(path_html)
     cleans = get_files(path_clean)
 
-    justextProcess(htmls)
+    cleanMetrics = dict()
+    for filename, datum in cleans.items():
+        cleanMetrics[os.path.basename(filename)] = getMetrics(datum)
+
+    argc = len(sys.argv)
+    if argc >= 2:
+        if 'jt' in sys.argv[1]:
+            pickle.dump(
+                justextProcess(htmls),
+                open('../resources/tempJT.save', mode='wb+')
+            )
+        if 'bp' in sys.argv[1]:
+            pickle.dump(
+                boilerpipeProcess(htmls),
+                open('../resources/tempBP.save', mode='wb+')
+            )
+
+    try:
+        fJT = open('../resources/tempJT.save', mode='rb')
+        outMetricsJT = pickle.load(fJT)
+        print('JT')
+        print(computeStastiscalMetrics(cleanMetrics, outMetricsJT))
+        print('##################################################')
+    except FileNotFoundError:
+        pass
+    try:
+        fBP = open('../resources/tempBP.save', mode='rb')
+        outMetricsBP = pickle.load(fBP)
+        print('BP')
+        print(computeStastiscalMetrics(cleanMetrics, outMetricsBP))
+        print('##################################################')
+    except FileNotFoundError:
+        pass
